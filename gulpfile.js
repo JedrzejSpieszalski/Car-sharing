8454c271507d574bb926132cd0c47a63a39b4d6b
JedrzejSpieszalski <66533368+JedrzejSpieszalski@users.noreply.github.com>
2022-07-20 00:34:50 +0200

    dodanie gulp

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+function server(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    cb();
+}
+
+
+function css() {
+return gulp.src('./scss/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        // outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+
+}
+
+function watch(cb) {
+    gulp.watch("./scss/**/*.scss", gulp.parallel(css));
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+    cb();
+}
+
+module.exports.css = css;
+module.exports.watch = watch;
+module.exports.server = server;
+module.exports.default = gulp.series(css, watch, server);
